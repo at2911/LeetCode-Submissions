13b6fd199372b5f00427d9263dd28297ec5ed6b6
Ankur Tiwari <tiwariankur2004@gmail.com>
2025-11-25 15:34:04 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.98%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    bool isPow7(int n){
-        if(n==1) return 1;
-        else
-            while(n!=1){
-            int r=n%7;
-            if(r!=0)return 0;
-            else n/=7;
-            }
+    // bool isPow7(int n){
+    //     if(n==1) return 1;
+    //     else
+    //         while(n!=1){
+    //         int r=n%7;
+    //         if(r!=0)return 0;
+    //         else n/=7;
+    //         }
         
-        return 1;
-    }
-    bool isPow3(int n){
-        if(n==1) return 1;
-        else
-            while(n!=1){
-            int r=n%3;
-            if(r!=0)return 0;
-            else n/=3;
-            }
+    //     return 1;
+    // }
+    // bool isPow3(int n){
+    //     if(n==1) return 1;
+    //     else
+    //         while(n!=1){
+    //         int r=n%3;
+    //         if(r!=0)return 0;
+    //         else n/=3;
+    //         }
         
-        return 1;
-    }
+    //     return 1;
+    // }
     int smallestRepunitDivByK(int k) {
-        // if(k==1) return 1;
+        // // if(k==1) return 1;
         unordered_set<int>s={0,2,4,5,6,8};
         if(s.count(k%10))return -1;
 
-        // int conunt=1;
-        if(isPow3(k)){
-            return k;
-        }
-        if(isPow7(k)){
-            return (k-k/7);
-        }
+        // // int conunt=1;
+        // if(isPow3(k)){
+        //     return k;
+        // }
+        // if(isPow7(k)){
+        //     return (k-k/7);
+        // }
         long long mod=0;
         for(int i=1;i<INT_MAX;i++){
             mod=(mod*10+1)%k;
