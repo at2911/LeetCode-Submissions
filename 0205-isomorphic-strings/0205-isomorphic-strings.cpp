a14c5ecef8b3a1da44edb1466d496d152d0868eb
Ankur Tiwari <tiwariankur2004@gmail.com>
2026-04-25 23:52:19 +0530

    Time: 3 ms (40.08%), Space: 9.5 MB (27.61%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+       for(int i=0;i<s.size();i++){
+        if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end()){
+            mp1[s[i]]=t[i];
+            mp2[t[i]]=s[i];
+        }
+        else{
+            if(mp1[s[i]]==t[i]&&mp2[t[i]]==s[i]){
+                continue;
+            }
+            else return 0;
+        }
+       }
+       return 1;
+        
+    }
+};
