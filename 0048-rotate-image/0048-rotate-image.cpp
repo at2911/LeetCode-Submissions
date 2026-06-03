e996e964536b0c86fc324affd02558a9e924a8ab
Ankur Tiwari <tiwariankur2004@gmail.com>
2026-06-03 10:36:58 +0530

    Time: 0 ms (100%), Space: 10.4 MB (7.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,6 @@ public:
         vector<vector<int>>copy=matrix;
         int n=matrix.size();
         int r=0;
-        
         for(int i=n-1;i>=0;i--){    
             for(int j=0;j<n;j++){
                 matrix[j][i]=copy[r][j];
